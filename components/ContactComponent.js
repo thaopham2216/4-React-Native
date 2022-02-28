0673b1e9fbef31bbfe367c56975003055df2b043
Thao Pham <thaopham2216@gmail.com>
2022-02-28 10:18:11 -0500

    Week 1 Workshop Assignment

---
+++ components/ContactComponent.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Text, ScrollView } from 'react-native';
+import { Card } from 'react-native-elements';
+
+class Contact extends Component {
+
+    static navigationOptions = {
+        title: 'Contact Us'
+    }
+    render() {
+        return(
+            <ScrollView>
+                <Card 
+                    featuredTitle='Contact Information'
+                    wrapperStyle={{margin: 20}}>
+                    <Text
+                        style={{ marginBottom: 10}}>
+                        1 Nucamp Way {'\n'}
+                        Seattle, WA 98001 {'\n'}
+                        U.S.A. {'\n'}
+
+                        Phone: 1-206-555-1234 {'\n'}
+                        Email: campsites@nucamp.co 
+                    </Text>
+                </Card>
+            </ScrollView>
+        )
+    }
+}
+
+export default Contact;
