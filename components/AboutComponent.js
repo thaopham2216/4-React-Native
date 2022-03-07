5fc38677c38d9b72bfaf2912783bc7ab85d62a6a
Thao Pham <thaopham2216@gmail.com>
2022-03-06 20:24:22 -0500

    Animatable

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,17 +1,11 @@
-/* import React, { Component } from 'react';
-import { Text, ScrollView } from 'react-native';
-import { ListItem } from 'react-native-elements';
-import { FlatList } from 'react-native-gesture-handler';
-import { Card } from 'react-native-elements';
-import { connect } from 'react-redux';
-import { baseUrl } from '../shared/baseUrl';
-*/
+
 import React, { Component } from 'react';
 import { ScrollView, Text, FlatList } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import Loading from './LoadingComponent';
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = state => {
     return {
@@ -58,24 +52,28 @@ class About extends Component {
         if (this.props.partners.errMess) {
             return (
                 <ScrollView>
-                    <Mission />
-                    <Card
-                        title='Community Partners'>
-                        <Text>{this.props.partners.errMess}</Text>
-                    </Card>
+                    <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                        <Mission />
+                        <Card
+                            title='Community Partners'>
+                            <Text>{this.props.partners.errMess}</Text>
+                        </Card>
+                    </Animatable.View>
                 </ScrollView>
             );
         }
         return (
             <ScrollView>
-                <Mission />
-                <Card title="Community Partners">
-                    <FlatList
-                        data={this.props.partners.partners}
-                        renderItem={renderPartner}
-                        keyExtractor={item => item.id.toString()}
-                    />
-                </Card>
+                <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                    <Mission />
+                    <Card title="Community Partners">
+                        <FlatList
+                            data={this.props.partners.partners}
+                            renderItem={renderPartner}
+                            keyExtractor={item => item.id.toString()}
+                        />
+                    </Card>
+                </Animatable.View>
             </ScrollView>
         )
     }
--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
 import { Text, ScrollView } from 'react-native';
 import { Card } from 'react-native-elements';
-
+import * as Animatable from 'react-native-animatable';
 class Contact extends Component {
 
     static navigationOptions = {
         title: 'Contact Us'
     }
     render() {
-        return(
+        return (
             <ScrollView>
-                <Card 
-                    featuredTitle='Contact Information'
-                    wrapperStyle={{margin: 20}}>
-                    <Text
-                        style={{ marginBottom: 10}}>
-                        1 Nucamp Way {'\n'}
-                        Seattle, WA 98001 {'\n'}
-                        U.S.A. {'\n'}
+                <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                    <Card
+                        featuredTitle='Contact Information'
+                        wrapperStyle={{ margin: 20 }}>
+                        <Text
+                            style={{ marginBottom: 10 }}>
+                            1 Nucamp Way {'\n'}
+                            Seattle, WA 98001 {'\n'}
+                            U.S.A. {'\n'}
 
-                        Phone: 1-206-555-1234 {'\n'}
-                        Email: campsites@nucamp.co 
-                    </Text>
-                </Card>
+                            Phone: 1-206-555-1234 {'\n'}
+                            Email: campsites@nucamp.co
+                        </Text>
+                    </Card>
+                </Animatable.View>
             </ScrollView>
         )
     }
